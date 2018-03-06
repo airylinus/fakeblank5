17d21b982c7c78501720dbbffcbc09865df4a0c7
zuomang <xiongzuomang@shenzhi.ai>
2018-03-06 10:40:25 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module "github.com/airylinus/blank5"
+
+require "rsc.io/quote" v1.5.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main // import "github.com/airylinus/blank5"
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Hello())
+}
