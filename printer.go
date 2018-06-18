896fe1cc6302c4a5a9cd34430aab343af1ccecc8
zuomang <xiongzuomang@shenzhi.ai>
2018-06-18 18:37:10 +0800

    version ++

--- printer.go
+++ printer.go
@@ -1,10 +1,6 @@
 package blank5 // import "github.com/airylinus/fakeblank5"
 
-import (
-	"fmt"
-)
-
 // Saying returns string
-func Saying() {
-	fmt.Println("I'm fake blank5 version 0.0.2")
+func Saying() string {
+	return "I'm fake blank5 version 0.0.3"
 }
